be1648fdd861d305031efe74a26f4e5b8308d23a
Angel-Carranza <64428856+Angel-Carranza@users.noreply.github.com>
2020-05-13 17:01:06 -0600

    Agregar archivos de proyecto.

---
+++ AprendiendoAUsarFor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendiendoAUsarFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anos, meses;
+
+            meses = 12;
+            for (anos = 1; anos <= 15; anos++)
+            {
+
+                Console.WriteLine("{0} años -> {1} meses", anos, meses);
+                meses = meses + 12;
+            }
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey();
+        }
+    }
+}
